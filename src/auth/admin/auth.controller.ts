ce535d22f2b19b073c944e222fa7126b7678c5ac
TemurMalik777 <your-email@example.com>
2025-05-13 12:16:31 +0500

    So'rvonoma

--- src/auth/admin/auth.controller.ts
+++ src/auth/admin/auth.controller.ts
@@ -1,7 +1,60 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Res,
+  UseGuards,
+} from "@nestjs/common";
 
-@Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+import { Response } from "express";
+import { CreateAdminDto } from "../../admin/dto/create-admin.dto";
+import { AdminAuthService } from "./auth.service";
+import { SignInDto } from "../dto/sign-in.dto";
+
+@Controller("auth-admin")
+export class AdminAuthController {
+  constructor(private readonly adminService: AdminAuthService) {}
+
+  @Post("sign-up")
+  signUp(@Body() createAdminDto: CreateAdminDto) {
+    return this.adminService.signUp(createAdminDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post("sign-in")
+  async singIn(
+    @Body() singInDto: SignInDto,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    return this.adminService.signIn(singInDto, res);
+  }
+
+  // @Get('sign-out')
+  // singout(
+  //   @Res({ passthrough: true }) res: Response,
+  // ) {
+  //   return this.adminService.signOut(refreshToken, res);
+  // }
+
+  // @ApiOperation({ summary: 'Tokenni yangilash (refresh token)' })
+  // @ApiResponse({
+  //   status: 200,
+  //   description: 'Yangilangan access va refresh tokenlar',
+  // })
+  // @ApiParam({ name: 'id', type: Number, description: 'Admin IDsi' })
+  // @ApiCookieAuth()
+  // @HttpCode(200)
+  // @Post(':id/refresh')
+  // refresh(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @CookieGetter('refresh_token') refreshToken: string,
+  //   @Res({ passthrough: true }) res: Response,
+  // ) {
+  //   return this.adminService.refreshToken(id, refreshToken, res);
+  // }
 }
--- src/auth/admin/auth.service.ts
+++ src/auth/admin/auth.service.ts
@@ -1,4 +1,155 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+// import { SignInDto } from '../dto/sing-in.dto';
+
+import { Response } from 'express';
+import * as bcrypt from 'bcrypt';
+// import { CreateStaffDto } from '../../staffs/dto/create-staff.dto';
+import { AdminService } from '../../admin/admin.service';
+import { Admin } from '../../admin/models/admin.model';
+import { CreateAdminDto } from '../../admin/dto/create-admin.dto';
+import { SignInDto } from '../dto/sign-in.dto';
 
 @Injectable()
-export class AuthService {}
+export class AdminAuthService {
+  constructor(
+    private readonly adminService: AdminService,
+    readonly jwtService: JwtService,
+  ) {}
+
+  private async generateTokenAdmin(admin: Admin) {
+    const payload = {
+      id: admin.id,
+      email: admin.email,
+      password: admin.hashed_password,
+      is_creater: admin.is_creater
+    };
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: process.env.ACCESS_TOKEN_KEY,
+        expiresIn: process.env.ACCESS_TOKEN_TIME,
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: process.env.REFRESH_TOKEN_KEY,
+        expiresIn: process.env.REFRESH_TOKEN_TIME,
+      }),
+    ]);
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+  async signUp(createAdminDto: CreateAdminDto) {
+    const admin = await this.adminService.findAdminByEmail(
+      createAdminDto.email,
+    );
+
+    if (admin) {
+      throw new ConflictException('Bunday emailli foydalanuvchi mavjud');
+    }
+
+    const newAdmin = await this.adminService.createAdmin(createAdminDto); // ✅
+    // console.log(newAdmin);
+
+    return { message: "Foydalanuvchi qo'shildi", adminId: newAdmin.id };
+  }
+
+  async signIn(signInDto: SignInDto, res: Response) {
+    const admin = await this.adminService.findAdminByEmail(signInDto.email);
+    if (!admin) {
+      throw new BadRequestException("Email yoki password noto'g'ri");
+    }
+
+    // console.log('Password:', signInDto.password);
+    // console.log('Hashed password:', staff);
+
+    const isValidPassword = await bcrypt.compare(
+      signInDto.password,
+      admin.hashed_password,
+    );
+    if (!isValidPassword) {
+      throw new BadRequestException("Email yoki password noto'g'ri");
+    }
+
+    const { accessToken, refreshToken } = await this.generateTokenAdmin(admin);
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      maxAge: Number(process.env.COOKIE_TIME),
+    });
+    admin.hashed_refresh_token = await bcrypt.hash(refreshToken, 7);
+    await admin.save();
+    res.status(200).send({
+      message: 'Tizimga xush keleibsiz',
+      refreshToken,
+    });
+  }
+
+  async signOut(refreshToken: string, res: Response) {
+    const adminData = await this.jwtService.verify(refreshToken, {
+      secret: process.env.REFRESH_TOKEN_KEY,
+    });
+
+    if (!adminData) {
+      throw new ForbiddenException('patient not verified');
+    }
+    const hashed_refresh_token = '';
+    await this.adminService.updateRefreshToken(
+      adminData.id,
+      hashed_refresh_token,
+    );
+
+    res.clearCookie('refresh_token');
+    const respnose = {
+      message: 'staff logged out successfully',
+    };
+    return respnose;
+  }
+
+  async refreshToken(adminId: number, refresh_token: string, res: Response) {
+    const decodeToken = await this.jwtService.decode(refresh_token);
+    console.log(adminId);
+    console.log(decodeToken['id']);
+
+    if (adminId !== decodeToken['id']) {
+      throw new ForbiddenException('Ruxsat etilmagan');
+    }
+    const admin = await this.adminService.findOneAdmin(adminId);
+
+    // console.log('Hashed token:', staff?.hashed_refresh_token);
+
+    if (!admin || !admin.hashed_refresh_token) {
+      throw new NotFoundException('staff not found');
+    }
+
+    const tokenMatch = await bcrypt.compare(
+      refresh_token,
+      admin.hashed_refresh_token,
+    );
+
+    if (!tokenMatch) {
+      throw new ForbiddenException('Forbidden');
+    }
+    const { accessToken, refreshToken } = await this.generateTokenAdmin(admin);
+
+    const hashed_refresh_token = await bcrypt.hash(refreshToken, 7);
+    await this.adminService.updateRefreshToken(admin.id, hashed_refresh_token);
+
+    res.cookie('refresh_token', refreshToken, {
+      maxAge: Number(process.env.COOKIE_TIME),
+      httpOnly: true,
+    });
+    const respnose = {
+      message: 'Staff refreshed',
+      patientId: admin.id,
+      access_token: accessToken,
+    };
+    return respnose;
+  }
+}
